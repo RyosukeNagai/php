3cdfd6824d10a2314b476529ae86379ba6478145
長井崚介 <ryooo@nagainoMacBook-Pro.local>
2021-09-03 21:09:41 +0900

    validation追加

---
+++ php基礎/form/validation.php
@@ -0,0 +1,16 @@
+<div class="error">
+  <?php 
+
+  function validation($request) { //$_POST連想配列
+
+    $errors = [];
+
+    if(empty($request['your_name'])){
+      $errors[] = '氏名は必須です';
+    }
+
+    return $errors;
+
+  }
+  ?>
+</div>
